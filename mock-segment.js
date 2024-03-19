c6c820a0a54df13df6c35310864fbac330835499
The SNEK ON ETH Team <137908309+systemcog@users.noreply.github.com>
2024-03-19 03:16:15 -0400

    Update mock-segment.js

--- mock-segment.js
+++ mock-segment.js
@@ -17,7 +17,7 @@ function onRequest(request, response, events) {
   });
   console.log(`${prefix}: Events received: ${eventDescriptions.join(', ')}`);
 
-  response.statusCode = 200;
+  response.statusCode = 201;
   response.end();
 }
 
